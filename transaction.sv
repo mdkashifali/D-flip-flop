d47353440efce88b4fe4335dfab5fe5a12f2a02d
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-24 17:30:05 +0530

    Create transaction.sv

---
+++ transaction.sv
@@ -0,0 +1,15 @@
+class transaction;
+  rand bit din;    // Define a random input bit "din"
+  bit dout;        // Define an output bit "dout"
+  
+  function transaction copy();
+    copy = new();   // Create a new transaction object
+    copy.din = this.din;  // Copy the input value
+    copy.dout = this.dout;  // Copy the output value
+  endfunction
+  
+  function void display(input string tag);
+    $display("[%0s] : DIN : %0b DOUT : %0b", tag, din, dout); // Display transaction information
+  endfunction
+  
+endclass
